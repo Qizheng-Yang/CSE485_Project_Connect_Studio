9fe376a5d5a8b5378781f35f0b9989cc4df95de8
neharao24 <nehavrao2016@gmail.com>
2025-03-06 09:47:33 -0700

    UI for Step navigation

--- src/components/StepNavigation.tsx
+++ src/components/StepNavigation.tsx
@@ -1,12 +1,52 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+const labels = [
+  'Info',
+  'Theme',
+  'Title Slides',
+  'Gallery',
+  'Effects',
+  'Music',
+  'Preview',
+  'Finalize',
+];
 
 function StepNavigation() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
-      {[1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
-        <Link key={num} to={`/step/${num}`} style={{ margin: '0 10px' }}>
-          <button>{num}</button>
-        </Link>
+      {[1, 2, 3, 4, 5, 6, 7, 8].map((num, index) => (
+        <div
+          key={num}
+          style={{
+            display: 'flex',
+            flexDirection: 'column', 
+            alignItems: 'center',
+            marginBottom: '30px', 
+            marginRight: '45px',
+          }}
+        >
+          {/* Button */}
+          <NavLink
+            to={`/step/${num}`}
+            className={({ isActive }) =>
+              isActive ? 'step_buttons active' : 'step_buttons'
+            }
+          >
+            {num}
+          </NavLink>
+
+          {/* Text Below Button */}
+          <span
+            style={{
+              marginTop: '15px', 
+              fontSize: '14px',
+              color: '#929396',
+              textAlign: 'center',
+            }}
+          >
+            {labels[index]}
+          </span>
+        </div>
       ))}
     </div>
   );
